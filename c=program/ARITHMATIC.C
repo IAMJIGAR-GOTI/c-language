be8114c4904c59744178335d34057effcebeee9e
IAMJIGAR-GOTI <137186650+IAMJIGAR-GOTI@users.noreply.github.com>
2023-10-10 10:35:35 +0530

    -m
    c program

---
+++ c=program/ARITHMATIC.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a=10,b=30,c,d,e,f;
+	clrscr();
+	printf("a=%d",a);
+	printf("\nb=%d",b);
+	c=a+b;
+	printf("\nc=%d",c);
+	d=b-a;
+	printf("\nd=%d",d);
+	e=a*b;
+	printf("\ne=%d",e);
+	f=b/a;
+	printf("\nf=%d",f);
+	return 0;
+
+}
---
+++ c=program/BIODATA.C
@@ -0,0 +1,18 @@
+`#include<stdio.h>
+#include<conio.h>
+main()
+{       clrscr();
+	printf("\tBIO-DATA\n\n");
+	printf("\nName      : Hardik Moradiya");
+	printf("\nBirthdate : 27th may 2001");
+	printf("\nEducation : B.SC");
+	printf("\nCollege   : sir p.p. institute of science");
+	printf("\nContact   : 9985454558");
+	printf("\nE-mail    : abv@123.gmail.com");
+	printf("\n\nFamily Information\n");
+	printf("\nFathername  : Thakarshibhai");
+	printf("\nSiblingname : Jaydip Moradiya");
+	printf("\nOccupation  : Daimond");
+	printf("\nAddress     : 36,haridarshan,katargam,surat.");
+	return 0;
+}
